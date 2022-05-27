67015f25386daa8802465482e35a7e1a282d5571
Bulbul <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-27 15:37:51 +0530

    Add files via upload

---
+++ meraki.py
@@ -0,0 +1,20 @@
+# i=1
+# while i<=100:
+#     if i%3==0:
+#         print("nav")
+#     elif i%7==0:
+#         print("gurukul") 
+#     elif i%3==0 and i%7==0:
+#         print("navgurukul") 
+#     else:
+#         print(i) 
+#     i+=1             
+
+# n=int(input("enter the number"))
+# sum=0
+# i=0
+# while i<n:
+#     b=int(input("enter the number"))
+#     sum+=b
+#     i+=1
+# print("sum of number is",sum)    
