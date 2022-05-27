548f2b0b77786b3d729eedd4ac9eca2ef8f23f29
Bulbul <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-27 15:38:31 +0530

    Add files via upload

---
+++ meraki 1 to 100 divisible by 7.py
@@ -0,0 +1,12 @@
+# num=int(input("enter the num"))
+# i=1
+# while i<=100:
+#     if i%num==0:
+#         print(i)
+#     i+=1   
+
+
+# for i in range(1,101,1):
+#     if i%7==0:
+#         print(i)
+ 
