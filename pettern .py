3f9ab7a9da7ef6c4a630aa3cedd305caa464176c
Bulbul <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-27 15:39:48 +0530

    Add files via upload

---
+++ pettern .py
@@ -0,0 +1,17 @@
+# i=0
+# while i<=5:
+#     j=0
+#     while j<i:
+#         if i%2!=0:
+#             if j%2!=0:
+#                 print(0,end=' ')
+#             else:
+#                 print(1,end=' ')
+#         else:
+#             if j%2==0:
+#                 print(0,end=' ')
+#             else:
+#                 print(1,end=' ')
+#         j+=1
+#     print()
+#     i+=1
