b69736fe4dd4ae00e6d86948dce3558be07d0693
Bulbul <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-27 15:40:54 +0530

    Add files via upload

---
+++ practice .py
@@ -0,0 +1,97 @@
+# n=int(input("enter the num"))
+# i=1
+# while i<=n:
+#     b=1
+#     while b<=n-i:
+#         print("-",end=" ")
+#         b+=1
+#     j=1
+#     while j<=i:
+#         print(j,end=" ")
+#         j+=1
+#     m=2
+#     while m<=i:
+#         print(j-m,end=" ")
+#         m+=1
+#     print()
+    # i+=1
+# n=int(input(" yjhjn "))
+# i=1
+# while i<=n:
+#     j=n
+#     while j>=i:
+#         print(j,end=" ")  
+#         j-=1
+#     print()
+#     i+=1
+          
+
+# i=1
+# while i<=7:
+#     j=1
+#     while j<=4:
+#         if j==1 or i-j==0 or i+j==8:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#         j+=1
+#     print()
+#     i+=1            
+
+          
+# a=65
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=6:
+#         if a==91:
+#             break
+#         print(chr(a),end=" ")
+#         j+=1
+#         a+=1
+#     print()
+#     i+=1    
+
+# i=1
+# while i<5:
+#     j=0
+#     while j<=5-i:
+#         print("*",end=" ")
+#         if j==4:
+#             break
+#         j=j+1
+#     print() 
+#     i+=1  
+# i=1
+# while i<=3:
+#     j=1
+#     while j<3+i:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1    
+
+
+
+# i=1
+# while i<=5:
+#     j=0
+#     while j<=5-i:
+#         print("*",end=" ")
+#         j=j+1
+#     print() 
+#     i+=1  
+# i=2
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1     
+
+# i=65
+# while i<=91:
+#     print(chr(i),end=" ")
+#     i+=1
+
