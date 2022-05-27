e3d4ed50fce2bc39a748c73dda5d50eeb3bc3d48
Bulbul <105332995+bulbulpatidar@users.noreply.github.com>
2022-05-27 15:33:13 +0530

    Add files via upload

---
+++ all perfect number.py
@@ -0,0 +1,19 @@
+from re import I
+
+
+# n=int(input("enter the number:-"))
+# a=1
+# while a<=n:
+#     s=0
+#     i=1
+#     while i<a:
+#         if a%i==0:
+#             s=s+i 
+#         i+=1
+#     if s==a:
+#         print("perfect number",a)
+#     else:
+#         print("not perfect number",a)  
+#     a+=1              
+
+
